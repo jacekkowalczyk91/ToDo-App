f837fc65921fffa27072571d1d89f5f9d600bfde
jacekkowalczyk91 <kowalczykjacek91@gmail.com>
2018-02-21 15:26:08 +0100

    add task done

--- src/App.js
+++ src/App.js
@@ -1,21 +1,24 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
+import AddTask from './Components/AddTask'
+
+import { Grid } from 'react-bootstrap'
+
+import {
+    BrowserRouter as Router, Route
+}from 'react-router-dom'
+
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <Router>
+                <Grid>
+                    <Route path='/' component={AddTask}/>
+                </Grid>
+            </Router>
+        )
+    }
 }
 
 export default App;
---
+++ src/Components/AddTask/AddTask.js
@@ -0,0 +1,57 @@
+import React from 'react'
+import {
+    Button,
+    FormGroup,
+    FormControl
+} from 'react-bootstrap'
+
+import {database} from '../../firebase'
+
+class AddTask extends React.Component {
+
+    state = {
+        id: '',
+        taskName: ''
+    }
+
+    handleTaskNameInputChange = (event) => {
+        this.setState({
+            taskName: event.target.value
+        });
+    }
+
+    handleAddTask = (event) => {
+        event.preventDefault()
+        const tasks = database().ref('tasks')
+        const task = {
+            name: this.state.taskName
+        }
+        tasks.push(task)
+            this.setState({
+                taskName: ''
+            })
+    }
+
+    render() {
+        return (
+            <div>
+                <form>
+                    <FormGroup>
+                        <FormControl
+                            type='text'
+                            placeholder='nazwa zadania'
+                            onChange={this.handleTaskNameInputChange}
+                            value={this.state.taskName}
+                        />
+                        <Button
+                            onClick={this.handleAddTask}
+                        >Zapisz</Button>
+                    </FormGroup>
+                </form>
+            </div>
+        )
+
+    }
+}
+
+export default AddTask
